b196d2e84ed13ba4ea296808df7a1e5f21a6436a
Rogério Antonio Galindo dos Santos <46696214+RogerioAGS@users.noreply.github.com>
2025-10-03 20:09:57 -0300

    Add files via upload

---
+++ br.com.estoque2/ServicoEstoque.java
@@ -0,0 +1,22 @@
+// É necessário importar a classe Produto, pois ela está em outro package
+import br.com.estoque.modelo.Produto;
+
+
+/**
+ * Classe de Serviço (Lógica): Contém regras de negócio.
+ * Fica no package br.com.estoque.servico.
+ */
+public class ServicoEstoque {
+   
+    /**
+     * Simula a adição de um produto ao estoque com uma regra simples.
+     */
+    public void adicionarProduto(ProdutoEstoque p, int valor) {
+        if (valor > 0) {
+            p.adicionarEstoque(valor);
+            System.out.println("LOG: Adicionado " + valor + " unidades de " + p.getNome() + ".");
+        } else {
+            System.out.println("LOG: Valor de adição deve ser positivo.");
+        }
+    }
+}
